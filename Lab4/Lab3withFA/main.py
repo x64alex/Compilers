a573341f07ea4ff5c7ffb7eef57382cf7925e519
Cantor Alexandru <alexcantor64@gmail.com>
2023-11-17 09:29:40 +0200

    Use fa for Lab3

---
+++ Lab4/Lab3withFA/main.py
@@ -0,0 +1,111 @@
+import st
+from fa import read_fa_from_file 
+
+def writePif(pif):
+    pifFile = open("PIF.out", "w")
+    for element in pif:
+        pifFile.write(str(element)+"\n")
+
+def writeST(identifiersST, integerST, stringST):
+    stFile = open("ST.out", "w")
+
+    stFile.write("Identifiers ST \n")
+    stFile.write(str(identifiersST))
+
+    stFile.write("\n\nInteger ST \n")
+    stFile.write(str(integerST))
+
+    stFile.write("\n\nString ST \n")
+    stFile.write(str(stringST))
+
+
+def writeFiles(pif, identifiersST, integerST, stringST):
+    writePif(pif)
+    writeST(identifiersST, integerST, stringST)
+
+def scanner(programFile, tokenFile):
+    delimiters = [",", ":", "(", ")", ">", "<","<=", ">=", "{", "}",  "&&", "||", "%", "==", "!=", "+", "-", "[", "]", "."]
+    tokens = []
+    pif = []
+    identifiersST = st.HashTable(10)
+    integerST = st.HashTable(10)
+    stringST = st.HashTable(10)
+
+    intFA = read_fa_from_file("int.in")
+    identifierFA = read_fa_from_file("identifier.in")
+
+    for token in tokenFile:
+        if token[-1] == "\n":
+            tokens.append(token[:-1])
+        else:
+            tokens.append(token)
+
+
+    for index, line in enumerate(programFile):
+        string = line
+        for delimiter in delimiters:
+            string = f" {delimiter} ".join(string.split(delimiter))
+
+        elements = string.split()
+        elIndex = 0
+        while elIndex < len(elements):
+            el = elements[elIndex]
+            if el == "//":
+                # if we have a comment ignore it
+                break
+            if el in tokens:
+                # if el is reserved word, operator or separator
+                pif.append((el, -1))
+            elif "'" in el:
+                # identify constant string
+                constructedString = el
+                elIndex += 1
+                el = elements[elIndex]
+                while "'" not in el:
+                    if el == "//":
+                        break
+                    constructedString = constructedString + " " + el
+                    if elIndex+1 == len(elements):
+                        writeFiles(pif, identifiersST, integerST, stringST)
+                        return f"lexical error at line {index+1} at identifier {el}"
+                    elIndex += 1
+                    el = elements[elIndex]
+
+                constructedString += " "+ el
+                position = stringST.insert(constructedString)
+                if position == -1:
+                    position = stringST.lookup(constructedString)
+                pif.append(("string", position))        
+            elif intFA.is_accepted(el):
+                # identify constant integer
+                position = integerST.insert(el)
+                if position == -1:
+                    position = integerST.lookup(el)
+                pif.append(("integer", position))
+            elif identifierFA.is_accepted(el):
+                # correct identifer
+                position = identifiersST.insert(el)
+                if position == -1:
+                    position = identifiersST.lookup(el)
+                pif.append(("id", position))
+            else:
+                writeFiles(pif, identifiersST, integerST, stringST)
+                return f"lexical error at line {index+1} at identifier {el}"
+            
+            elIndex +=1
+    writeFiles(pif, identifiersST, integerST, stringST)
+    return "lexically correct"
+
+program1 = open("p1.cat")
+program1er = open("p1err.cat")
+program2 = open("p2.cat")
+program3 = open("p3.cat")
+program3string = open("p3string.cat")
+
+tokens = open("token.in")
+
+# print(scanner(program1, tokens))
+# print(scanner(program1er, tokens))
+# print(scanner(program2, tokens))
+# print(scanner(program3, tokens))
+# print(scanner(program3string, tokens))
---
+++ Lab4/Lab3withFA/st.py
@@ -0,0 +1,74 @@
+class HashTable:
+    # Separate chaining
+	def __init__(self, size):
+		self.__size = size
+		self.__hash_table = [[] for _ in range(self.__size)]
+
+	def get_size(self):
+		return self.__size
+
+	def __get_hash(self, key):
+        # h(x) = (sum(x)^2 / len(x)) % size
+		return (sum(ord(char) for char in key) **2 // len(key)) % self.__size
+	
+	def exists(self, key):
+		hashed_key = self.__get_hash(key)
+		return key in self.__hash_table[hashed_key]
+
+	def insert(self, key):
+		hashed_key = self.__get_hash(key)
+		bucket = self.__hash_table[hashed_key]
+
+		if self.exists(key) is False :
+			bucket.append(key)
+			position = bucket.index(key)
+			return (hashed_key, position)
+		return -1
+
+	def lookup(self, key):
+		hashed_key = self.__get_hash(key)
+		bucket = self.__hash_table[hashed_key]
+
+		if key in bucket:
+			position = bucket.index(key)
+			return (hashed_key, position)
+		else:
+			return -1
+
+	def delete(self, key):
+		hashed_key = self.__get_hash(key)
+		bucket = self.__hash_table[hashed_key]
+
+		if key in bucket:
+			position = bucket.index(key)
+			bucket.pop(position)
+			return (hashed_key, position)
+		else:
+			return -1
+
+	def __str__(self):
+		return "".join(str(item) for item in self.__hash_table)
+
+symbol_table = HashTable(10)
+
+# print("Insert " + "x" + " at position: " + str(symbol_table.insert('x')))
+# print("Insert " + "xy" + " at position: " + str(symbol_table.insert('xy')))
+# print("Insert " + "xy" + " at position: " + str(symbol_table.insert('xy')))
+# print("Insert " + "xyz" + " at position: " + str(symbol_table.insert('xyz')))
+# print("Insert " + "variable" + " at position: " + str(symbol_table.insert('variable')))
+# print("Insert " + "miau" + " at position: " + str(symbol_table.insert('miau')))
+
+# print()
+# print("Contains " + "xy" + " at position: " + str(symbol_table.lookup('xy')))
+# print("Remove " + "xy" + " at position: " + str(symbol_table.delete('xy')))
+
+# print()
+# print("Contains " + "variable" + " at position: " + str(symbol_table.lookup('variable')))
+# print("Remove " + "variable" + " at position: " + str(symbol_table.delete('variable')))
+
+# print()
+# print("Contains " + "variable" + " at position: " + str(symbol_table.lookup('variable')))
+# print("Remove " + "variable" + " at position: " + str(symbol_table.delete('variable')))
+
+# print()
+# print(symbol_table)
